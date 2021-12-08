e943c0eaa67a951cdd26799012478648404db907
Nora Szabo <noraster@kali.net>
2021-12-08 11:24:32 +0100

    Wrong

---
+++ first.py
@@ -0,0 +1,15 @@
+# #print ('Hello world')
+
+# a = int(input ('Give me a number: '))
+
+# ##if 9 < a < 100:
+#   #  print ('It is a two digit number')
+
+# #if a % 2 == 0:
+#    # print('Even')
+# #else:
+#    # print('Odd')
+
+for i in range(1, a + 1):
+    print(i)
+
